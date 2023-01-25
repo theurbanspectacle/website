0d25746ec9b78bcaeffd10f8eeb2a4ceff971468
Genesis (Oliver) G <107947533+theurbanspectacle@users.noreply.github.com>
2023-01-25 13:57:25 -0800

    changes to about me

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -5,9 +5,9 @@ export default class AboutMe extends React.Component {
   render() {
     return (
       <div className="about-me">
-        <p>Hello, I'm Oliver Martin. Formerly known as Genesis. I was born and raised in San Diego, CA. My coding journey started in my late teenage years when MySpace and Tumblr was at its peak. I wondered how everything worked, so I tinkered with code until I could figure out how to personalize my pages. I enjoyed putting the pieces together. After the popularity of those websites died, I stopped coding. I found out that careers existed out of what I was doing, so I made the switch.</p>
+        <p>Hello, I'm Oliver Martin. Formerly known as Genesis. I was born and raised in San Diego, and currently reside in Orange County, CA. My coding journey started in my late teens when MySpace and Tumblr was at its peak. I wondered how everything worked, so I tinkered with code until I could figure out how everything functioned. I enjoyed putting the pieces together. After the popularity of those websites died, I stopped coding. I found out as an adult that careers existed with this, so I happily made the switch into this field.</p>
           
-        <p>Besides all of that, I love art in general. I started my childhood with drawing, then I got inspired in my early teens from friends to pick up the guitar, sing, and make music. In my late teens, I picked up a little bit of dance, and also fell in love with Photography. </p> 
+        <p>Besides coding, I love art and design in general. I started my childhood with drawing, then I got inspired in my early teens to pick up the guitar, sing, and make music. In my late teens, I started to dance, and also fell in love with photography. Most recently, I've tried acting and worked behind the scenes for short films thanks to my connections. Please click the contact link to get a hold of me and thank you for stopping by! </p> 
       </div>
     );
   }
